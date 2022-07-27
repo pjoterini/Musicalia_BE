24529e16b01f24962506e593fb53ca3b1a6e4885
pjoterini <piotrekjan.gorski@gmail.com>
2022-07-27 12:42:43 +0200

    redeploy4

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
-const dotenv = require('dotenv').config()
+const env = require('dotenv')
+env.config()
 
 const mongoose = require('mongoose')
-mongoose.connect(process.env.CONNECTIONSTRING)
+mongoose.connect(process.env.DATABASE_URL)
 const db = mongoose.connection
 db.on('error', error => console.error(error))
 db.once('open', () => console.log('connected to Mongoose'))
