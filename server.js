c3e64351b074364e844f681eb2507102de001f4d
pjoterini <piotrekjan.gorski@gmail.com>
2022-12-01 14:35:43 +0100

    fixin textencoder 1

--- server.js
+++ server.js
@@ -1,34 +1,38 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const mongoose = require('mongoose')
-mongoose.connect(process.env.DATABASE_URL)
-const db = mongoose.connection
-db.on('error', error => console.error(error))
-db.once('open', () => console.log('connected to Mongoose'))
+var util = require("util");
+var encoder = new util.TextEncoder("utf-8");
 
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-const methodOverride = require('method-override')
+const mongoose = require("mongoose");
+mongoose.connect(process.env.DATABASE_URL);
+const db = mongoose.connection;
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("connected to Mongoose"));
 
-app.set('view engine', 'ejs')
-app.set('views', __dirname + '/views')
-app.set('layout', 'layouts/layout')
-app.use(expressLayouts)
-app.use(methodOverride('_method'))
-app.use(express.static('public'))
-app.use(express.urlencoded({ limit: '10mb', extended: false}))
-app.use(express.json())
+const express = require("express");
+const app = express();
+const expressLayouts = require("express-ejs-layouts");
+const methodOverride = require("method-override");
 
-app.use('/', require('./routes/index'))
-app.use('/artists', require('./routes/artists'))
-app.use('/songs', require('./routes/songs'))
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
+app.set("layout", "layouts/layout");
+app.use(expressLayouts);
+app.use(methodOverride("_method"));
+app.use(express.static("public"));
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
+app.use(express.json());
 
+app.use("/", require("./routes/index"));
+app.use("/artists", require("./routes/artists"));
+app.use("/songs", require("./routes/songs"));
 
-app.get('/test', async (req, res) => {
-    const data = await db.collection('artists').find().toArray()
-    console.log(data)
-    res.send('sup')
-})
+app.get("/test", async (req, res) => {
+  const data = await db.collection("artists").find().toArray();
+  console.log(data);
+  res.send("sup");
+});
 
-app.listen(process.env.PORT || 3000, () => console.log('http://localhost:3000'))
+app.listen(process.env.PORT || 3000, () =>
+  console.log("http://localhost:3000")
+);
