dfb1f3ce41021486f6eb41a5262d4ae0859f0433
pjoterini <piotrekjan.gorski@gmail.com>
2022-12-01 12:36:40 +0100

    redeploy

--- routes/index.js
+++ routes/index.js
@@ -1,82 +1,98 @@
-const url = require('url')
-const express = require('express')
-const router = express.Router()
-const Song = require('../models/song')
-const Artist = require('../models/artist')
+const url = require("url");
+const express = require("express");
+const router = express.Router();
+const Song = require("../models/song");
+const Artist = require("../models/artist");
+// redeploy
 
+const fetch = require("node-fetch");
 
-const fetch = require('node-fetch') 
+router.get("/", async (req, res) => {
+  //  API PROXY
 
-router.get('/', async (req, res) => {
+  let songs;
+  let artists;
+  let articlesList;
 
-    //  API PROXY
-    
-    let songs 
-    let artists
-    let articlesList
+  try {
+    let date = new Date(new Date().setDate(new Date().getDate() - 30))
+      .toISOString()
+      .slice(0, 10);
+    let apiUrl = `https://newsapi.org/v2/everything?q=music&from=${date}&sortBy=relevancy&apiKey=${process.env.API_KEY}`;
 
-    try {
-        let date = new Date(new Date().setDate(new Date().getDate()-30)).toISOString().slice(0, 10)
-        let apiUrl = `https://newsapi.org/v2/everything?q=music&from=${date}&sortBy=relevancy&apiKey=${process.env.API_KEY}`
+    const apiRes = await fetch(apiUrl);
+    const apiData = await apiRes.json();
+    articlesList = await apiData.articles.slice(0, 8);
 
-        const apiRes = await fetch(apiUrl)
-        const apiData = await apiRes.json()
-        articlesList = await apiData.articles.slice(0, 8)
+    artists = await Artist.find().limit(6).exec();
+    songs = await Song.find().populate("artist").limit(10).exec();
+  } catch (err) {
+    console.log(err);
+    songs = [];
+    artists = [];
+  }
+  res.render("index", {
+    articlesList: articlesList,
+    songs: songs,
+    artists: artists,
+  });
+});
 
-        artists = await Artist.find().limit(6).exec()
-        songs = await Song.find().populate('artist').limit(10).exec()
-    } catch(err) {
-        console.log(err)
-        songs = []
-        artists = []
+router.post("/", async function (req, res) {
+  let songs;
+  let artists;
+  let articlesList;
+  let reqBodyVar = "music";
+
+  try {
+    let date = new Date(new Date().setDate(new Date().getDate() - 30))
+      .toISOString()
+      .slice(0, 10);
+
+    if (req.body.music == "music") {
+      reqBodyVar = "music";
     }
-    res.render('index', {
-        articlesList: articlesList,
-        songs: songs,
-        artists: artists
-    })
-})
-
-
-router.post('/', async function(req, res) {
-    
-    let songs 
-    let artists
-    let articlesList
-    let reqBodyVar = 'music'
-
-    try {
-        let date = new Date(new Date().setDate(new Date().getDate()-30)).toISOString().slice(0, 10)
-
-        if (req.body.music == 'music') {reqBodyVar = 'music'}
-        if (req.body.songs == 'songs') {reqBodyVar = 'songs song chart top'}
-        if (req.body.festivals == 'festivals') {reqBodyVar = 'festival tour band music'}
-        if (req.body.classical == 'classical') {reqBodyVar = 'classical concert opera music'}
-        if (req.body.artists == 'artists') {reqBodyVar = 'bands artist artists band music'}
-        if (req.body.artists == 'artists') {reqBodyVar = 'soundtrack movie game music'}
-        if (req.body.retro == 'retro') {reqBodyVar = 'old-music retro-music'}
-        if (req.body.alternative == 'alternative') {reqBodyVar = 'music alternative'}
-
-        let apiUrl = `https://newsapi.org/v2/everything?q=${reqBodyVar}&from=${date}&sortBy=relevancy&apiKey=${process.env.API_KEY}`
-
-        const apiRes = await fetch(apiUrl)
-        const apiData = await apiRes.json()
-        articlesList = await apiData.articles.slice(0, 8)
-
-        artists = await Artist.find().limit(6).exec()
-        songs = await Song.find().populate('artist').limit(10).exec()
-        
-    } catch(err) {
-        console.log(err)
-        songs = []
-        artists = []
+    if (req.body.songs == "songs") {
+      reqBodyVar = "songs song chart top";
     }
-    
-    res.render('index', {
-        articlesList: articlesList,
-        songs: songs,
-        artists: artists
-    })
-})
+    if (req.body.festivals == "festivals") {
+      reqBodyVar = "festival tour band music";
+    }
+    if (req.body.classical == "classical") {
+      reqBodyVar = "classical concert opera music";
+    }
+    if (req.body.artists == "artists") {
+      reqBodyVar = "bands artist artists band music";
+    }
+    if (req.body.artists == "artists") {
+      reqBodyVar = "soundtrack movie game music";
+    }
+    if (req.body.retro == "retro") {
+      reqBodyVar = "old-music retro-music";
+    }
+    if (req.body.alternative == "alternative") {
+      reqBodyVar = "music alternative";
+    }
 
-module.exports = router
+    let apiUrl = `https://newsapi.org/v2/everything?q=${reqBodyVar}&from=${date}&sortBy=relevancy&apiKey=${process.env.API_KEY}`;
+
+    const apiRes = await fetch(apiUrl);
+    const apiData = await apiRes.json();
+    articlesList = await apiData.articles.slice(0, 8);
+
+    artists = await Artist.find().limit(6).exec();
+    songs = await Song.find().populate("artist").limit(10).exec();
+  } catch (err) {
+    console.log(err);
+    songs = [];
+    artists = [];
+  }
+
+  res.render("index", {
+    articlesList: articlesList,
+    songs: songs,
+    artists: artists,
+  });
+});
+
+module.exports = router;
